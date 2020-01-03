3707f1cd8c3f9ce653753c1838807694007efe94
dhaval <d.shah@tum.de>
2020-01-03 22:44:28 +0000

    Changed losses

--- dvn/losses.py
+++ dvn/losses.py
@@ -4,17 +4,25 @@ import torch
 import torch.tensor as tensor
 import torch.nn as nn
 import torch.optim as optim
+from torch.autograd import Variable
 import numpy as np
 
 class Dice_Loss(nn.Module):
     def __init__(self):
         super().__init__()
         
-    def forward(self, inputs, targets):
-        numerator = 2. * torch.sum(inputs*targets)
-        denominator = torch.sum(inputs + targets)
+    def forward(self, outputs, targets):
+        _, pred = torch.max(outputs, 1)
         
-        return 1 - numerator/denominator
+        pred = pred.contiguous().view(-1)
+        targets = targets.contiguous().view(-1)
+        
+        numerator = 2. * torch.sum(pred * targets)
+        denominator = torch.sum(pred + targets)
+        
+        loss = Variable((1. - (numerator + 1)/(denominator + 1)), requires_grad=True)
+        
+        return loss
         
         
 # Doesn't work yet
@@ -24,13 +32,13 @@ class Weighted_CCE(nn.Module):
         super().__init__()
         self.classes = classes
         
-    def forward(self, inputs, targets):
+    def forward(self, outputs, targets):
         loss = nn.CrossEntropyLoss(reduction='none')
-        L = loss(inputs, targets)
+        L = loss(outputs, targets)
         y_max, y_true_p = torch.max(targets, 1)
         
         for c in range(self.classes):
-            c_true = torch.eq(y_true_p, c).type(inputs.dtype)
+            c_true = torch.eq(y_true_p, c).type(outputs.dtype)
             w = 1. / (torch.sum(c_true))
             print(c_true.shape)
             print(L.shape)
@@ -46,23 +54,23 @@ class Weighted_CCE_FPR(nn.Module):
         self.classes = classes
         self.threshold = threshold
         
-    def forward(inputs, targets):
+    def forward(outputs, targets):
         L1 = nn.CrossEntropyLoss(reduction='none')
-        L = L1(inputs, targets)
+        L = L1(outputs, targets)
         y_max, y_true_p = torch.max(targets)
-        y_pred_probs, y_pred_bin = torch.max(inputs)
+        y_outputs_probs, y_outputs_bin = torch.max(outputs)
 
         for c in range(classes):
             # element-wise equality
-            c_true = torch.eq(y_true_p, c).type(inputs.dtype).unsqueeze(0)
+            c_true = torch.eq(y_true_p, c).type(outputs.dtype).unsqueeze(0)
             w = 1. / (torch.sum(c_true))
             C = torch.sum(L*w*c_true) if c == 0 else C + torch.sum(L*w*c_true)
 
             # FP rate correction calculation
-            # False prediction
-            c_false_p = torch.ne(y_true_p, c) * torch.eq(y_pred_bin, c)
+            # False outputsiction
+            c_false_p = torch.ne(y_true_p, c) * torch.eq(y_outputs_bin, c)
             # Calculate gamma
-            gamma = 0.5 + (torch.sum(torch.abs((c_false_p * y_pred_probs) - 0.5)) / (torch.sum(c_false_p)))
+            gamma = 0.5 + (torch.sum(torch.abs((c_false_p * y_outputs_probs) - 0.5)) / (torch.sum(c_false_p)))
             wc = w * gamma
             C = C + torch.sum(L * c_false_p * wc)
 
